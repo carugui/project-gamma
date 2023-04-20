a249c567263aae4fcae6433a5d2846be0d524795
Rosa <=>
2023-04-20 20:47:11 +0200

    implementando scss

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './scss/style.scss'
 import Home from './views/Home'
 import Contact from './views/Contact'
 
