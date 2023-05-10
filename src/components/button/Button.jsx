b9de66646d826d5d16ab95ca2cfc2e794c9074db
Rosa <=>
2023-05-10 20:57:04 +0200

    arreglo botones

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -4,6 +4,11 @@ function Button() {
     return (
         <>
           <button>Soy un botón</button>
+          <button className='btn--secondary'>Soy un botón</button>
+          <button className='btn--cta'>Soy un botón</button>
+          <a href="xxx" className='btn'>Soy un link</a>
+          <a href="xxx" className='btn btn--secondary'>Soy un link</a>
+          <a href="xxx" className='btn btn--cta'>Soy un link</a>
         </>
     )
 }
