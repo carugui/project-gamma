79f025e4442a912a15a2d5524d60aebc9067d0d8
Carla C <carla.cc.cg@hotmail.com>
2023-05-29 11:25:21 +0200

    added opne and close function to menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,22 +1,42 @@
 import './Topbar.scss';
 import { Link, Route, Routes } from 'react-router-dom'
 import Home from '../../views/Home'
-
+import React, { useEffect, useState, useRef } from "react";
 
 function Topbar() {
- 
+
+  const [open, setOpen] = useState(false);
+
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e)=>{
+      if(!menuRef.current?.contains(e.target)){
+        setOpen(false);
+        console.log(menuRef.current);
+      }      
+    };
+
+    document.addEventListener("mousedown", handler);
+    
+
+    return() =>{
+      document.removeEventListener("mousedown", handler);
+    }
+
+  });
   return (
     <>
       <header className='topbar'>
 
         <nav>
-          Menú
-          <ul>
+          <button onClick={()=>{setOpen(!open)}}>Menú</button>
+          <ul className={`nav__ul ${open ? 'nav__ul--show' : ''}`}>
             <li>
-              <a href="">Proyectos</a>
+              <a href="" >Proyectos</a>
             </li>
             <li>
-              <a href="">Sobre mí</a>
+              <a href="" >Sobre mí</a>
             </li>
             <li>
               <a href="">Correo</a>
