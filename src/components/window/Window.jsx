a23fe71cfc824c012ab6abd72213ca16cb64970c
Carla C <carla.cc.cg@hotmail.com>
2023-06-05 12:45:07 +0200

    añadido las funciones de componente ventana

--- src/components/window/Window.jsx
+++ src/components/window/Window.jsx
@@ -1,1 +1,26 @@
 import './Window.scss';
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Draggable from 'react-draggable'
+
+function Window(props) {
+    const nodeRef = React.useRef(null);
+    return (
+        <Draggable nodeRef={nodeRef}>
+            <div className={`window ${props.size === '' ? '' : `window__size-${props.size}`}`} ref={nodeRef}>
+
+                <header>
+                    <h2 className='h5'>{props.title}</h2>
+                </header>
+
+                <div className='window__content'>
+                    {props.content}
+                </div>
+                
+            </div>
+        </Draggable>
+
+    )
+}
+
+export default Window;
--- src/views/ProjectsList.jsx
+++ src/views/ProjectsList.jsx
@@ -2,25 +2,29 @@ import { Link } from "react-router-dom";
 import { PROJECTS } from "../../public/projects";
 import WebDesktop from "../components/desktopItems/WebDesktop";
 import Button from "../components/button/Button";
+import Window from "../components/window/Window";
 
 export default function ProjectsList() {
     const listado = PROJECTS;
 
     return <>
-    {listado.map(proyecto =>
 
-        <div>
+        <Window title='Proyectos' size="medium" content =
+        
+            {listado.map(proyecto =>
 
-            <WebDesktop
-                featured={proyecto.featured}
-                text={proyecto.title}
-                goTo={ '/projects/' + proyecto.id}
-            >
-            {/* {proyecto.links.map(link => <Button type="secondary">{ link.name }</Button>)} */}
-                
-            </WebDesktop>
+                <WebDesktop
+                    featured={proyecto.featured}
+                    text={proyecto.title}
+                    goTo={ '/proyectos/' + proyecto.id}
+                >
+                {/* {proyecto.links.map(link => <Button type="secondary">{ link.name }</Button>)} */}
+                    
+                </WebDesktop>
 
-        </div>)}
+            )}
+        
+        />
 
     </>
 }
