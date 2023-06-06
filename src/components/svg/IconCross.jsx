4198b0b16972a1ba0f1da9cdd594d739c896a04b
Carla C <carla.cc.cg@hotmail.com>
2023-06-06 21:03:17 +0200

    Creado svg icono cerrar

---
+++ src/components/svg/IconCross.jsx
@@ -0,0 +1,11 @@
+function IconCross(props) {
+    return (
+
+        <svg width="14" height="14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="m2.121 2 10 10M2 12 12 2" stroke="#004999" strokeWidth="3" strokeLinecap="round"/>
+        </svg>
+
+    )
+}
+
+export default IconCross;
