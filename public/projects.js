38534ef90e2afad3ccd4a0bf3f6c777bf65d9ac2
Carla C <carla.cc.cg@hotmail.com>
2023-06-16 13:47:13 +0200

    Eliminado proyecto placeholder

--- public/projects.js
+++ public/projects.js
@@ -41,30 +41,5 @@ export const PROJECTS = [
         ],
         "year": "2023",
         "role": "Frontend Developer"
-    },
-    {
-        "id": "chat",
-        "title": "Chat app",
-        "description": "Lorem ipsum es estem rosare",
-        "image": "https://png.pngtree.com/png-vector/20190927/ourlarge/pngtree-chat-icon-png-image_1757855.jpg",
-        "links": [
-            {
-                "url": "https://github.com/jroji",
-                "name": "github",
-                "img": "https://cdn-icons-png.flaticon.com/512/25/25231.png"
-            },
-            {
-                "url": "https://github.com/jroji",
-                "name": "github",
-                "img": "https://cdn-icons-png.flaticon.com/512/25/25231.png"
-            },
-            {
-                "url": "https://github.com/jroji",
-                "name": "github",
-                "img": "https://cdn-icons-png.flaticon.com/512/25/25231.png"
-            }
-        ],
-        "year": "2023",
-        "role": "Main Designer"
     }
 ]
