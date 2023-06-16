9abac5b52cb2a4406ecda79b564d75865ac981ce
Carla C <carla.cc.cg@hotmail.com>
2023-06-16 15:42:21 +0200

    modificado draggable mobile

--- src/components/desktopItems/FileDesktop.jsx
+++ src/components/desktopItems/FileDesktop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import IconFile from "../svg/IconFile";
 import Draggable from 'react-draggable'
@@ -6,19 +6,54 @@ import './DesktopItems.scss'
 
 function FileDesktop(props) {
     const nodeRef = React.useRef(null);
-    return (
-        <Draggable nodeRef={nodeRef}>
-            <div className="desktopItem" ref={nodeRef}>
 
-                <Link to={props.goTo} onClick={props.clickTo} target={props.target ? props.target : '_self'}>
+    // Comprobar el tamaño de la ventana, si es menor de 800px se usa la librería Draggable, si no, no se usa.
+    const [isDraggable, setIsDraggable] = useState(false);
+
+    useEffect(() => {
+        const windowsSize = () => {
+            const isMobile = window.innerWidth <= 800;
+            setIsDraggable(!isMobile);
+        };
+
+        windowsSize();
+        window.addEventListener('resize', windowsSize);
+
+        return () => {
+            window.removeEventListener('resize', windowsSize);
+        };
+        
+    }, []);
+
+    return (
+        <div>
+            {isDraggable && (
+                <Draggable nodeRef={nodeRef}>
+                    <div className="desktopItem" ref={nodeRef}>
+
+                        <Link to={props.goTo} onClick={props.clickTo} target={props.target ? props.target : '_self'}>
+                        
+                            <IconFile></IconFile>
+                            <h1 className="h5">{props.text}</h1>
+                        
+                        </Link>
+                        
+                    </div>
+                </Draggable>
+            )}
+            {!isDraggable &&
+                <div className="desktopItem" ref={nodeRef}>
+
+                    <Link to={props.goTo} onClick={props.clickTo} target={props.target ? props.target : '_self'}>
+                    
+                        <IconFile></IconFile>
+                        <h1 className="h5">{props.text}</h1>
+                    
+                    </Link>
                 
-                    <IconFile></IconFile>
-                    <h1 className="h5">{props.text}</h1>
-                
-                </Link>
-                
-            </div>
-        </Draggable>
+                </div>
+            }
+        </div>
 
     )
 }
--- src/components/desktopItems/MailDesktop.jsx
+++ src/components/desktopItems/MailDesktop.jsx
@@ -1,19 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import IconMail from "../svg/IconMail";
 import Draggable from 'react-draggable'
 import './DesktopItems.scss'
 
 function MailDesktop(props) {
     const nodeRef = React.useRef(null);
+
+    // Comprobar el tamaño de la ventana, si es menor de 800px se usa la librería Draggable, si no, no se usa.
+    const [isDraggable, setIsDraggable] = useState(false);
+
+    useEffect(() => {
+        const windowsSize = () => {
+            const isMobile = window.innerWidth <= 800;
+            setIsDraggable(!isMobile);
+        };
+
+        windowsSize();
+        window.addEventListener('resize', windowsSize);
+
+        return () => {
+            window.removeEventListener('resize', windowsSize);
+        };
+        
+    }, []);
+
     return (
-        <Draggable nodeRef={nodeRef}>
-            <div className="desktopItem" ref={nodeRef}>
-            
-                <a href="mailto:soy.carugui@gmail.com"><IconMail></IconMail></a>
-                <a href="mailto:soy.carugui@gmail.com"><h1 className="h5">{props.text}</h1></a>
+
+        <div>
+
+            {isDraggable && (
+                <Draggable nodeRef={nodeRef}>
+                    <div className="desktopItem" ref={nodeRef}>
+                    
+                        <a href="mailto:soy.carugui@gmail.com"><IconMail></IconMail></a>
+                        <a href="mailto:soy.carugui@gmail.com"><h1 className="h5">{props.text}</h1></a>
+                        
+                    </div>
+                </Draggable>
+            )}
+            {!isDraggable &&
+                <div className="desktopItem" ref={nodeRef}>
+                    
+                    <a href="mailto:soy.carugui@gmail.com"><IconMail></IconMail></a>
+                    <a href="mailto:soy.carugui@gmail.com"><h1 className="h5">{props.text}</h1></a>
                 
-            </div>
-        </Draggable>
+                </div>
+            }
+        </div>
 
     )
 }
