f4d83e2784957e257bdb8e09bca16832053d9d2f
Carla C <carla.cc.cg@hotmail.com>
2023-06-16 13:47:34 +0200

    Modificado draggable para mobile

--- src/components/desktopItems/FolderDesktop.jsx
+++ src/components/desktopItems/FolderDesktop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import IconFolder from "../svg/IconFolder";
 import Draggable from 'react-draggable'
@@ -6,20 +6,58 @@ import './DesktopItems.scss'
 
 function FolderDesktop(props) {
     const nodeRef = React.useRef(null);
-    return (
-        <Draggable nodeRef={nodeRef}>
+
+    const [isDraggable, setIsDraggable] = useState(false);
+
+
+    // Comprobar el tamaño de la ventana, si es menor de 800px se usa la librería Draggable, si no, no se usa.
+    useEffect(() => {
+        const windowsSize = () => {
+            const isMobile = window.innerWidth <= 800;
+            setIsDraggable(!isMobile);
+        };
+
+        windowsSize();
+        window.addEventListener('resize', windowsSize);
+
+        return () => {
+            window.removeEventListener('resize', windowsSize);
+        };
         
-            <div className="desktopItem" ref={nodeRef}>
+    }, []);
 
-                <Link to={props.goTo} onClick={props.clickTo} target={props.target ? props.target : '_self'}>
-                    <IconFolder></IconFolder>
-                    <h1 className="h5">{props.text}</h1>
-                </Link>
+    return (
+
+        <div>
+
+            {isDraggable && (
                 
-            </div>
-        </Draggable>
+                <Draggable nodeRef={nodeRef}>
+                
+                    <div className="desktopItem" ref={nodeRef}>
 
-    )
-}
+                        <Link to={props.goTo} onClick={props.clickTo} target={props.target ? props.target : '_self'}>
+                            <IconFolder></IconFolder>
+                            <h1 className="h5">{props.text}</h1>
+                        </Link>
+                        
+                    </div>
+                </Draggable>
+            )}
+            {!isDraggable &&
+                <div className="desktopItem" ref={nodeRef}>
+
+                    <Link to={props.goTo} onClick={props.clickTo} target={props.target ? props.target : '_self'}>
+                        <IconFolder></IconFolder>
+                        <h1 className="h5">{props.text}</h1>
+                    </Link>
+                        
+                </div>
+            }
+
+        </div>
+
+    );
+};
 
 export default FolderDesktop;
--- src/components/window/Window.jsx
+++ src/components/window/Window.jsx
@@ -1,26 +1,68 @@
 import './Window.scss';
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Draggable from 'react-draggable'
 import IconCross from '../svg/IconCross';
 
 function Window(props) {
     const nodeRef = React.useRef(null);
+
+    // Comprobar el tamaño de la ventana, si es menor de 800px se usa la librería Draggable, si no, no se usa.
+    const [isDraggable, setIsDraggable] = useState(false);
+
+    useEffect(() => {
+        const windowsSize = () => {
+            const isMobile = window.innerWidth <= 800;
+            setIsDraggable(!isMobile);
+        };
+
+        windowsSize();
+        window.addEventListener('resize', windowsSize);
+
+        return () => {
+            window.removeEventListener('resize', windowsSize);
+        };
+        
+    }, []);
+
+
     return (
-        <Draggable nodeRef={nodeRef}>
-            <div className={`window ${props.optionalClasses} ${props.size === '' ? '' : `window__size-${props.size}`} ${props.position === '' ? '' : `window__position--${props.position}`}`} ref={nodeRef}>
 
-                <header>
-                    <IconCross className="iconcross" goTo={props.goTo} clickTo={props.clickTo}/>
-                    <h2 className='h5'>{props.title}</h2>
-                </header>
+        <div>
 
-                <div className={`window__content ${props.type === '' ? '' : `window__content--${props.type}`}`}>
-                    {props.content}
+            {isDraggable && (
+                <Draggable nodeRef={nodeRef}>
+                    <div className={`window ${props.optionalClasses} ${props.size === '' ? '' : `window__size-${props.size}`} ${props.position === '' ? '' : `window__position--${props.position}`}`} ref={nodeRef}>
+
+                        <header>
+                            <IconCross className="iconcross" goTo={props.goTo} clickTo={props.clickTo}/>
+                            <h2 className='h5'>{props.title}</h2>
+                        </header>
+
+                        <div className={`window__content ${props.type === '' ? '' : `window__content--${props.type}`}`}>
+                            {props.content}
+                        </div>
+                        
+                    </div>
+                </Draggable>
+            )}
+            {!isDraggable &&
+
+                <div className={`window ${props.optionalClasses} ${props.size === '' ? '' : `window__size-${props.size}`} ${props.position === '' ? '' : `window__position--${props.position}`}`} ref={nodeRef}>
+
+                    <header>
+                        <IconCross className="iconcross" goTo={props.goTo} clickTo={props.clickTo}/>
+                        <h2 className='h5'>{props.title}</h2>
+                    </header>
+
+                    <div className={`window__content ${props.type === '' ? '' : `window__content--${props.type}`}`}>
+                        {props.content}
+                    </div>
+
                 </div>
-                
-            </div>
-        </Draggable>
+            }
+            
+        </div>
 
     )
 }
