5baa16cd317cbe2c645d0b49683e347a6b360502
Carla C <carla.cc.cg@hotmail.com>
2023-09-18 10:25:57 +0200

    añadido links en sobre mi

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -25,6 +25,8 @@ export default function About({ onClose }) {
                     <hr className="rounded"></hr>
                     <p>Después de graduarme en la carrera en Arte y Diseño en la Escuela Superior de Arte y Tecnología en 2022 y aprender multitud de disciplinas como branding, modelado 3D y maquetación, decididí ampliar mis conocimientos centrándome en el diseño y desarrollo web front-end.</p>
                     <p>Recientemente me he graduado en el postgrado ID3 especializado en UX/UI, marketing digital y desarrollo web en la ESAT.</p>
+                    <p>Aquí tienes mi <a href='../../public/documents/curriculum-carla-cardona.pdf' target='_blank'>curriculum</a>, y si estás interesado en el Diseño Gráfico aquí tienes mi <a href='../../public/documents/portfolio-laboral-carla-cardona.pdf' target='_blank'>portfolio de diseño</a>.</p>
+                    <p>¡Si quieres saber más de mí no dudes en preguntarme lo que sea en < a href="mailto:soy.carugui@gmail.com">soy.carugui@gmail.com</a>!</p>
 
                 </div>
             }
